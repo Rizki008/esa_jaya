76e597384d0dac85de9a6241daea7bca7e63e738
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-26 23:26:32 +0700

    menambahkan js datables, marcdown, load gambar

--- application/views/backend/v_footer.php
+++ application/views/backend/v_footer.php
@@ -152,10 +152,56 @@
 <script src="<?= base_url() ?>backend/dist/assets/vendors/jvectormap/jquery-jvectormap-2.0.3.min.js" type="text/javascript"></script>
 <script src="<?= base_url() ?>backend/dist/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
 <script src="<?= base_url() ?>backend/dist/assets/vendors/jvectormap/jquery-jvectormap-us-aea-en.js" type="text/javascript"></script>
+<script src="<?= base_url() ?>backend/dist/assets/vendors/DataTables/datatables.min.js" type="text/javascript"></script>
+<script src="<?= base_url() ?>backend/dist/assets/vendors/bootstrap-markdown/js/bootstrap-markdown.js" type="text/javascript"></script>
 <!-- CORE SCRIPTS-->
 <script src="<?= base_url() ?>backend/dist/assets/js/app.min.js" type="text/javascript"></script>
 <!-- PAGE LEVEL SCRIPTS-->
 <script src="<?= base_url() ?>backend/dist/assets/js/scripts/dashboard_1_demo.js" type="text/javascript"></script>
+<!-- PAGE LEVEL SCRIPTS-->
+<script type="text/javascript">
+    $(function() {
+        $('#example-table').DataTable({
+            pageLength: 10,
+            //"ajax": './assets/demo/data/table_data.json',
+            /*"columns": [
+                { "data": "name" },
+                { "data": "office" },
+                { "data": "extn" },
+                { "data": "start_date" },
+                { "data": "salary" }
+            ]*/
+        });
+    })
+</script>
+
+<!-- summernote -->
+<script type="text/javascript">
+    $(function() {
+        $('#summernote').summernote();
+        $('#summernote_air').summernote({
+            airMode: true
+        });
+    });
+</script>
+
+
+<!-- load gambar -->
+<script>
+    function bacaGambar(input) {
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function(e) {
+                $('#gambar_load').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $('#preview_gambar').change(function() {
+        bacaGambar(this);
+    });
+</script>
 </body>
 
 </html>
