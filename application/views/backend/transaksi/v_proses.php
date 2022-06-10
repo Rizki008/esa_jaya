db827f52307f7e8c9ab97f585944e1cde6a908d3
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-10 18:26:26 +0700

    menambahkan fitur transaksi

---
+++ application/views/backend/transaksi/v_proses.php
@@ -0,0 +1,89 @@
+<div class="page-heading">
+    <h1 class="page-title">Basic Tables</h1>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="index.html"><i class="la la-home font-20"></i></a>
+        </li>
+        <li class="breadcrumb-item">Basic Tables</li>
+    </ol>
+</div>
+<div class="page-content fade-in-up">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="ibox">
+                <div class="ibox-head">
+                    <div class="ibox-title">Contextual classes</div>
+                </div>
+                <div class="ibox-body">
+                    <table class="table table-hover">
+                        <caption>Optional table caption.</caption>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="active">
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>02/08/2017</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>04/10/2017</td>
+                            </tr>
+                            <tr class="info">
+                                <td>3</td>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>08/11/2017</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>04/10/2017</td>
+                            </tr>
+                            <tr class="success">
+                                <td>3</td>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>08/11/2017</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>04/10/2017</td>
+                            </tr>
+                            <tr class="warning">
+                                <td>3</td>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>08/11/2017</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>04/10/2017</td>
+                            </tr>
+                            <tr class="danger">
+                                <td>3</td>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>08/11/2017</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
