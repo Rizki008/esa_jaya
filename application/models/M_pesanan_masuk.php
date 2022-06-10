772eaeefced63c7561f6fb29b27321f6dfd8a264
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-10 18:26:49 +0700

    menambahkan proses pesanan masuk

---
+++ application/models/M_pesanan_masuk.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_pesanan_masuk extends CI_Model
+{
+
+    public function FunctionName()
+    {
+        # code...
+    }
+}
