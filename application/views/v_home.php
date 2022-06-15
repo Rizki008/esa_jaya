ec40e97d11e0574bf7a9d7dd74833a1afcb23406
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-15 07:08:47 +0700

    menambahkan fitur tambah keranjang produk

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -91,74 +91,61 @@
 <!-- ****** Top Catagory Area End ****** -->
 
 <!-- ****** Quick View Modal Area Start ****** -->
-<div class="modal fade" id="quickview" tabindex="-1" role="dialog" aria-labelledby="quickview" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <button type="button" class="close btn" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
 
-            <div class="modal-body">
-                <div class="quickview_body">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-12 col-lg-5">
-                                <div class="quickview_pro_img">
-                                    <img src="img/product-img/product-1.jpg" alt="">
+<?php foreach ($produk as $value) { ?>
+    <div class="modal fade" id="quickview<?= $value->id_produk ?>" tabindex="-1" role="dialog" aria-labelledby="quickview" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <button type="button" class="close btn" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <?php
+                echo form_open('home/modal_produk/' . $value->id_produk);
+                ?>
+                <div class="modal-body">
+                    <div class="quickview_body">
+                        <div class="container">
+                            <?php echo form_open('belanja/add');
+                            echo form_hidden('id', $value->id_produk);
+                            echo form_hidden('qty', '1');
+                            echo form_hidden('price', $value->harga - $value->diskon);
+                            echo form_hidden('name', $value->nama_produk);
+                            echo form_hidden('redirect_page', str_replace('index.php/', '', current_url()));
+                            ?>
+                            <div class="row">
+                                <div class="col-12 col-lg-5">
+                                    <div class="quickview_pro_img">
+                                        <img src="<?= base_url('uploads/produk/' . $value->gambar) ?>" alt="">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-12 col-lg-7">
-                                <div class="quickview_pro_des">
-                                    <h4 class="title">Boutique Silk Dress</h4>
-                                    <div class="top_seller_product_rating mb-15">
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                <div class="col-12 col-lg-7">
+                                    <div class="quickview_pro_des">
+                                        <h4 class="title"><?= $value->nama_produk ?></h4>
+                                        <h5 class="price">Rp. <?= $value->diskon ?> <span>Rp. <?= number_format($value->harga, 0) ?></span></h5>
+                                        <p><?= $value->deskripsi ?></p>
+                                        <a href="<?= site_url('home/detail_produk/' . $value->id_produk); ?>">View Full Product Details</a>
                                     </div>
-                                    <h5 class="price">$120.99 <span>$130</span></h5>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia expedita quibusdam aspernatur, sapiente consectetur accusantium perspiciatis praesentium eligendi, in fugiat?</p>
-                                    <a href="#">View Full Product Details</a>
-                                </div>
-                                <!-- Add to Cart Form -->
-                                <form class="cart" method="post">
-                                    <div class="quantity">
-                                        <span class="qty-minus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty ) &amp&ampqty &gt  ) effect.value--;return false;"><i class="fa fa-minus" aria-hidden="true"></i></span>
-
-                                        <input type="number" class="qty-text" id="qty" step="1" min="1" max="12" name="quantity" value="1">
-
-                                        <span class="qty-plus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty )) effect.value++;return false;"><i class="fa fa-plus" aria-hidden="true"></i></span>
-                                    </div>
-                                    <button type="submit" name="addtocart" value="5" class="cart-submit">Add to cart</button>
-                                    <!-- Wishlist -->
-                                    <div class="modal_pro_wishlist">
-                                        <a href="wishlist.html" target="_blank"><i class="ti-heart"></i></a>
-                                    </div>
-                                    <!-- Compare -->
-                                    <div class="modal_pro_compare">
-                                        <a href="compare.html" target="_blank"><i class="ti-stats-up"></i></a>
-                                    </div>
-                                </form>
-
-                                <div class="share_wf mt-30">
-                                    <p>Share With Friend</p>
-                                    <div class="_icon">
-                                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                        <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-                                        <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                                    <!-- Add to Cart Form -->
+                                    <form class="cart" method="post">
+                                        <div class="quantity">
+                                            <input type="number" class="qty-text" id="qty" step="1" min="1" max="<?= $value->stock ?>" name="stock" value="1">
+                                        </div>
+                                        <button type="submit" name="addtocart" value="5" class="cart-submit">Add to cart</button>
+                                    </form>
+                                    <div class="share_wf mt-30">
                                     </div>
                                 </div>
                             </div>
+                            <?php echo form_close() ?>
                         </div>
                     </div>
                 </div>
+                <?php echo form_close() ?>
             </div>
         </div>
     </div>
-</div>
-<!-- ****** Quick View Modal Area End ****** -->
+    <!-- ****** Quick View Modal Area End ****** -->
+<?php } ?>
 
 <!-- ****** New Arrivals Area Start ****** -->
 <section class="new_arrivals_area section_padding_100_0 clearfix">
@@ -172,144 +159,56 @@
         </div>
     </div>
 
-    <div class="karl-projects-menu mb-100">
-        <div class="text-center portfolio-menu">
-            <button class="btn active" data-filter="*">ALL</button>
-            <button class="btn" data-filter=".women">WOMAN</button>
-            <button class="btn" data-filter=".man">MAN</button>
-            <button class="btn" data-filter=".access">ACCESSORIES</button>
-            <button class="btn" data-filter=".shoes">shoes</button>
-            <button class="btn" data-filter=".kids">KIDS</button>
-        </div>
-    </div>
 
     <div class="container">
         <div class="row karl-new-arrivals">
-
-            <!-- Single gallery Item Start -->
-            <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig" data-wow-delay="0.2s">
-                <!-- Product Image -->
-                <div class="product-img">
-                    <img src="img/product-img/product-1.jpg" alt="">
-                    <div class="product-quicview">
-                        <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
+            <?php if (count($produk) > 0) : ?>
+                <?php foreach ($produk as $value) : ?>
+                    <!-- Single gallery Item Start -->
+                    <?php echo form_open('belanja/add');
+                    echo form_hidden('id', $value->id_produk);
+                    echo form_hidden('qty', '1');
+                    echo form_hidden('price', $value->harga - $value->diskon);
+                    echo form_hidden('name', $value->nama_produk);
+                    echo form_hidden('redirect_page', str_replace('index.php/', '', current_url()));
+                    ?>
+                    <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig" data-wow-delay="0.2s">
+                        <!-- Product Image -->
+                        <div class="product-img">
+                            <img src="<?= base_url('uploads/produk/' . $value->gambar) ?>" alt="">
+                            <div class="product-quicview">
+                                <a href="#" data-toggle="modal" data-target="#quickview<?= $value->id_produk ?>"><i class="ti-plus"></i></a>
+                            </div>
+                        </div>
+                        <!-- Product Description -->
+                        <div class="product-description">
+                            <h4 class="product-price">Rp. <?= number_format($value->harga, 0) ?></h4>
+                            <p><?= $value->nama_produk ?></p>
+                            <!-- Add to Cart -->
+                            <a href="#" class="add-to-cart-btn">ADD TO CART</a>
+                        </div>
                     </div>
-                </div>
-                <!-- Product Description -->
-                <div class="product-description">
-                    <h4 class="product-price">$39.90</h4>
-                    <p>Jeans midi cocktail dress</p>
-                    <!-- Add to Cart -->
-                    <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-                </div>
-            </div>
-
-            <!-- Single gallery Item Start -->
-            <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig" data-wow-delay="0.3s">
-                <!-- Product Image -->
-                <div class="product-img">
-                    <img src="img/product-img/product-2.jpg" alt="">
-                    <div class="product-quicview">
-                        <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-                    </div>
-                </div>
-                <!-- Product Description -->
-                <div class="product-description">
-                    <h4 class="product-price">$39.90</h4>
-                    <p>Jeans midi cocktail dress</p>
-                    <!-- Add to Cart -->
-                    <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-                </div>
-            </div>
-
-            <!-- Single gallery Item Start -->
-            <div class="col-12 col-sm-6 col-md-4 single_gallery_item access wow fadeInUpBig" data-wow-delay="0.4s">
-                <!-- Product Image -->
-                <div class="product-img">
-                    <img src="img/product-img/product-3.jpg" alt="">
-                    <div class="product-quicview">
-                        <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-                    </div>
-                </div>
-                <!-- Product Description -->
-                <div class="product-description">
-                    <h4 class="product-price">$39.90</h4>
-                    <p>Jeans midi cocktail dress</p>
-                    <!-- Add to Cart -->
-                    <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-                </div>
-            </div>
-
-            <!-- Single gallery Item Start -->
-            <div class="col-12 col-sm-6 col-md-4 single_gallery_item shoes wow fadeInUpBig" data-wow-delay="0.5s">
-                <!-- Product Image -->
-                <div class="product-img">
-                    <img src="img/product-img/product-4.jpg" alt="">
-                    <div class="product-quicview">
-                        <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-                    </div>
-                </div>
-                <!-- Product Description -->
-                <div class="product-description">
-                    <h4 class="product-price">$39.90</h4>
-                    <p>Jeans midi cocktail dress</p>
-                    <!-- Add to Cart -->
-                    <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-                </div>
-            </div>
-
-            <!-- Single gallery Item Start -->
-            <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig" data-wow-delay="0.6s">
-                <!-- Product Image -->
-                <div class="product-img">
-                    <img src="img/product-img/product-5.jpg" alt="">
-                    <div class="product-quicview">
-                        <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-                    </div>
-                </div>
-                <!-- Product Description -->
-                <div class="product-description">
-                    <h4 class="product-price">$39.90</h4>
-                    <p>Jeans midi cocktail dress</p>
-                    <!-- Add to Cart -->
-                    <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-                </div>
-            </div>
-
-            <!-- Single gallery Item -->
-            <div class="col-12 col-sm-6 col-md-4 single_gallery_item kids man wow fadeInUpBig" data-wow-delay="0.7s">
-                <!-- Product Image -->
-                <div class="product-img">
-                    <img src="img/product-img/product-6.jpg" alt="">
-                    <div class="product-quicview">
-                        <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-                    </div>
-                </div>
-                <!-- Product Description -->
-                <div class="product-description">
-                    <h4 class="product-price">$39.90</h4>
-                    <p>Jeans midi cocktail dress</p>
-                    <!-- Add to Cart -->
-                    <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-                </div>
-            </div>
+                    <?php echo form_close() ?>
+                <?php endforeach; ?>
+            <?php else : ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
 <!-- ****** New Arrivals Area End ****** -->
 
 <!-- ****** Offer Area Start ****** -->
-<section class="offer_area height-700 section_padding_100 bg-img" style="background-image: url(img/bg-img/bg-5.jpg);">
+<section class="offer_area height-700 section_padding_100 bg-img" style="background-image: url(<?= base_url('uploads/produk/' . $best_produk->gambar) ?>);">
     <div class="container h-100">
         <div class="row h-100 align-items-end justify-content-end">
             <div class="col-12 col-md-8 col-lg-6">
                 <div class="offer-content-area wow fadeInUp" data-wow-delay="1s">
-                    <h2>White t-shirt <span class="karl-level">Hot</span></h2>
-                    <p>* Free shipping until 25 Dec 2017</p>
+                    <h2><?= $best_produk->nama_produk ?> <span class="karl-level">Hot</span></h2>
+                    <p>* <?= $best_produk->nama_diskon ?></p>
                     <div class="offer-product-price">
-                        <h3><span class="regular-price">$25.90</span> $15.90</h3>
+                        <h3><span class="regular-price">Rp. <?= number_format($best_produk->harga, 0) ?></span> Rp. <?= number_format($best_produk->diskon, 0) ?></h3>
                     </div>
-                    <a href="#" class="btn karl-btn mt-30">Shop Now</a>
+                    <a href="<?= site_url('home/detail_produk/' . $best_produk->id_produk); ?>" class="btn karl-btn mt-30">Shop Now</a>
                 </div>
             </div>
         </div>
