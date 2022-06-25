81f442840a6e42a4d8a4927fd8933071b2a21657
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-25 11:44:59 +0700

    menambahkan produklain nya

--- application/views/frontend/detail/v_produk.php
+++ application/views/frontend/detail/v_produk.php
@@ -140,71 +140,6 @@
 </section>
 <!-- <<<<<<<<<<<<<<<<<<<< Single Product Details Area End >>>>>>>>>>>>>>>>>>>>>>>>> -->
 
-<!-- ****** Quick View Modal Area Start ****** -->
-<div class="modal fade" id="quickview" tabindex="-1" role="dialog" aria-labelledby="quickview" aria-hidden="true">
-	<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-		<div class="modal-content">
-			<button type="button" class="close btn" data-dismiss="modal" aria-label="Close">
-				<span aria-hidden="true">&times;</span>
-			</button>
-			<div class="modal-body">
-				<div class="quickview_body">
-					<div class="container">
-						<div class="row">
-							<div class="col-12 col-lg-5">
-								<div class="quickview_pro_img">
-									<img src="img/product-img/product-1.jpg" alt="">
-								</div>
-							</div>
-							<div class="col-12 col-lg-7">
-								<div class="quickview_pro_des">
-									<h4 class="title">Boutique Silk Dress</h4>
-									<div class="top_seller_product_rating mb-15">
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									</div>
-									<h5 class="price">$120.99 <span>$130</span></h5>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia expedita quibusdam aspernatur, sapiente consectetur accusantium perspiciatis praesentium eligendi, in fugiat?</p>
-									<a href="#">View Full Product Details</a>
-								</div>
-								<!-- Add to Cart Form -->
-								<form class="cart" method="post">
-									<div class="quantity">
-										<input type="number" class="qty-text" id="qty2" step="1" min="1" max="12" name="quantity" value="1">
-									</div>
-									<button type="submit" name="addtocart" value="5" class="cart-submit">Add to cart</button>
-									<!-- Wishlist -->
-									<div class="modal_pro_wishlist">
-										<a href="wishlist.html" target="_blank"><i class="ti-heart"></i></a>
-									</div>
-									<!-- Compare -->
-									<div class="modal_pro_compare">
-										<a href="compare.html" target="_blank"><i class="ti-stats-up"></i></a>
-									</div>
-								</form>
-
-								<div class="share_wf mt-30">
-									<p>Share With Friend</p>
-									<div class="_icon">
-										<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-										<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-										<a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-										<a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<!-- ****** Quick View Modal Area End ****** -->
-
 <section class="you_may_like_area clearfix">
 	<div class="container">
 		<div class="row">
@@ -215,100 +150,34 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-12">
-				<div class="you_make_like_slider owl-carousel">
+			<?php if (count($produk_lainnya) > 0) : ?>
+				<?php foreach ($produk_lainnya as $product) : ?>
 
-					<!-- Single gallery Item -->
-					<div class="single_gallery_item">
-						<!-- Product Image -->
-						<div class="product-img">
-							<img src="img/product-img/product-1.jpg" alt="">
-							<div class="product-quicview">
-								<a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
+					<div class="col-12">
+						<div class="you_make_like_slider owl-carousel">
+
+							<!-- Single gallery Item -->
+							<div class="single_gallery_item">
+								<!-- Product Image -->
+								<div class="product-img">
+									<img src="<?php echo base_url('uploads/produk/' . $product->gambar); ?>" alt="">
+									<div class="product-quicview">
+									</div>
+								</div>
+								<!-- Product Description -->
+								<div class="product-description">
+									<h4 class="product-price">Rp <?php echo number_format($product->harga); ?></h4>
+									<p><?php echo anchor('belanja/add/' . $product->id_produk . '/', $product->nama_produk); ?></p>
+									<!-- Add to Cart -->
+									<a href="<?= site_url('home/detail_produk/' . $value->id_produk); ?>" class="add-to-cart-btn">Detail Produk</a>
+								</div>
 							</div>
-						</div>
-						<!-- Product Description -->
-						<div class="product-description">
-							<h4 class="product-price">$39.90</h4>
-							<p>Jeans midi cocktail dress</p>
-							<!-- Add to Cart -->
-							<a href="#" class="add-to-cart-btn">ADD TO CART</a>
+
+
 						</div>
 					</div>
-
-					<!-- Single gallery Item -->
-					<div class="single_gallery_item">
-						<!-- Product Image -->
-						<div class="product-img">
-							<img src="img/product-img/product-2.jpg" alt="">
-							<div class="product-quicview">
-								<a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-							</div>
-						</div>
-						<!-- Product Description -->
-						<div class="product-description">
-							<h4 class="product-price">$39.90</h4>
-							<p>Jeans midi cocktail dress</p>
-							<!-- Add to Cart -->
-							<a href="#" class="add-to-cart-btn">ADD TO CART</a>
-						</div>
-					</div>
-
-					<!-- Single gallery Item -->
-					<div class="single_gallery_item">
-						<!-- Product Image -->
-						<div class="product-img">
-							<img src="img/product-img/product-3.jpg" alt="">
-							<div class="product-quicview">
-								<a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-							</div>
-						</div>
-						<!-- Product Description -->
-						<div class="product-description">
-							<h4 class="product-price">$39.90</h4>
-							<p>Jeans midi cocktail dress</p>
-							<!-- Add to Cart -->
-							<a href="#" class="add-to-cart-btn">ADD TO CART</a>
-						</div>
-					</div>
-
-					<!-- Single gallery Item -->
-					<div class="single_gallery_item">
-						<!-- Product Image -->
-						<div class="product-img">
-							<img src="img/product-img/product-4.jpg" alt="">
-							<div class="product-quicview">
-								<a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-							</div>
-						</div>
-						<!-- Product Description -->
-						<div class="product-description">
-							<h4 class="product-price">$39.90</h4>
-							<p>Jeans midi cocktail dress</p>
-							<!-- Add to Cart -->
-							<a href="#" class="add-to-cart-btn">ADD TO CART</a>
-						</div>
-					</div>
-
-					<!-- Single gallery Item -->
-					<div class="single_gallery_item">
-						<!-- Product Image -->
-						<div class="product-img">
-							<img src="img/product-img/product-5.jpg" alt="">
-							<div class="product-quicview">
-								<a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-							</div>
-						</div>
-						<!-- Product Description -->
-						<div class="product-description">
-							<h4 class="product-price">$39.90</h4>
-							<p>Jeans midi cocktail dress</p>
-							<!-- Add to Cart -->
-							<a href="#" class="add-to-cart-btn">ADD TO CART</a>
-						</div>
-					</div>
-				</div>
-			</div>
+				<?php endforeach; ?>
+			<?php endif; ?>
 		</div>
 	</div>
 </section>
