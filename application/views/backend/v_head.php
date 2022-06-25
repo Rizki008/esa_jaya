77225626b399cf334f3edec0e7c6ffdf49a0fea3
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-25 10:53:22 +0700

    menambahakn js chart

--- application/views/backend/v_head.php
+++ application/views/backend/v_head.php
@@ -2,19 +2,21 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width initial-scale=1.0">
-    <title>TB. Esa Jaya | <?= $title ?></title>
-    <!-- GLOBAL MAINLY STYLES-->
-    <link href="<?= base_url() ?>backend/dist/assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="<?= base_url() ?>backend/dist/assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-    <link href="<?= base_url() ?>backend/dist/assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
-    <!-- PLUGINS STYLES-->
-    <link href="<?= base_url() ?>backend/dist/assets/vendors/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
-    <link href="<?= base_url() ?>backend/dist/assets/vendors/DataTables/datatables.min.css" rel="stylesheet" />
-    <link href="<?= base_url() ?>backend/dist/assets/vendors/bootstrap-markdown/css/bootstrap-markdown.min.css" rel="stylesheet" />
-    <!-- THEME STYLES-->
-    <link href="<?= base_url() ?>backend/dist/assets/css/main.min.css" rel="stylesheet" />
-    <!-- PAGE LEVEL STYLES-->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width initial-scale=1.0">
+	<title>TB. Esa Jaya | <?= $title ?></title>
+	<!-- GLOBAL MAINLY STYLES-->
+	<link href="<?= base_url() ?>backend/dist/assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="<?= base_url() ?>backend/dist/assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="<?= base_url() ?>backend/dist/assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
+	<!-- PLUGINS STYLES-->
+	<link href="<?= base_url() ?>backend/dist/assets/vendors/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
+	<link href="<?= base_url() ?>backend/dist/assets/vendors/DataTables/datatables.min.css" rel="stylesheet" />
+	<link href="<?= base_url() ?>backend/dist/assets/vendors/bootstrap-markdown/css/bootstrap-markdown.min.css" rel="stylesheet" />
+	<!-- THEME STYLES-->
+	<link href="<?= base_url() ?>backend/dist/assets/css/main.min.css" rel="stylesheet" />
+	<!-- PAGE LEVEL STYLES-->
+	<script src="<?= base_url() ?>backend/chart/dist/Chart.min.js"></script>
+	<script src="<?= base_url() ?>backend/chart/samples/utils.js"></script>
 </head>
