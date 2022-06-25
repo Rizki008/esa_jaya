62748b518f0f5585104a34d0d442d3d44266f2df
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-25 10:49:58 +0700

    menambahkan proses katalog

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,12 +5,56 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Home extends CI_Controller
 {
 
-    public function index()
-    {
-        $data = array(
-            'title' => 'Katalog Esa Jaya',
-            'isi' => 'v_home'
-        );
-        $this->load->view('frontend/v_wrapper', $data, FALSE);
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_home');
+	}
+
+	public function index()
+	{
+		$data = array(
+			'title' => 'Katalog',
+			'produk' => $this->m_home->produk(),
+			'best_produk' => $this->m_home->best_produk(),
+			'isi' => 'v_home'
+		);
+		$this->load->view('frontend/v_wrapper', $data, FALSE);
+	}
+
+	public function kategori($id_kategori)
+	{
+		$kategori = $this->m_home->kategori($id_kategori);
+		$data = array(
+			'title' => $kategori->nama_kategori,
+			'best_produk' => $this->m_home->best_produk(),
+			'diskon' => $this->m_home->diskon(),
+			'produk' => $this->m_home->produk_all($id_kategori),
+			'isi' => 'v_home',
+		);
+		$this->load->view('frontend/v_wrapper', $data, FALSE);
+	}
+
+	public function modal_produk($id_produk = NULL)
+	{
+		$data = array(
+			'id_produk' => $id_produk,
+		);
+		$this->m_home->produk($data);
+		$this->session->set_flashdata('pesan', 'Kategori Berhasil Diedit!!!');
+		redirect('home');
+	}
+
+	public function detail_produk($id_produk = null)
+	{
+		$data = array(
+			'title' => 'Detail Produk',
+			'produk' => $this->m_home->detail_produk($id_produk),
+			'gambar' => $this->m_home->gambar_produk($id_produk),
+			'diskon' => $this->m_home->diskon(),
+			'produk_lainnya' => $this->m_home->produk_lainnya($id_produk),
+			'isi' => 'frontend/detail/v_produk'
+		);
+		$this->load->view('frontend/v_wrapper', $data, FALSE);
+	}
 }
