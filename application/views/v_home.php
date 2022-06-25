09be17d1c5fef997b3ee9ed7857f83acaaf0d885
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-25 11:43:40 +0700

    menghapus tampilan tidak penting dan menambahkan info produk laris

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -21,74 +21,25 @@
 <!-- ****** Welcome Slides Area Start ****** -->
 <section class="welcome_area">
 	<div class="welcome_slides owl-carousel">
-		<!-- Single Slide Start -->
-		<div class="single_slide height-800 bg-img background-overlay" style="background-image: url(img/bg-img/bg-1.jpg);">
-			<div class="container h-100">
-				<div class="row h-100 align-items-center">
-					<div class="col-12">
-						<div class="welcome_slide_text">
-							<h6 data-animation="bounceInDown" data-delay="0" data-duration="500ms">* Only today we offer free shipping</h6>
-							<h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Fashion Trends</h2>
-							<a href="#" class="btn karl-btn" data-animation="fadeInUp" data-delay="1s" data-duration="500ms">Shop Now</a>
+		<?php foreach ($best_deal_product_transaksi as $key => $value) { ?>
+			<!-- Single Slide Start -->
+			<div class="single_slide height-800 bg-img background-overlay" style="background-image: url(<?= base_url('uploads/produk/' . $value->gambar) ?>);">
+				<div class="container h-100">
+					<div class="row h-100 align-items-center">
+						<div class="col-12">
+							<div class="welcome_slide_text">
+								<h6 data-animation="bounceInDown" data-delay="0" data-duration="500ms">* Produk Paling Laris</h6>
+								<h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms"><?= $value->nama_produk ?></h2>
+								<a href="#" class="btn karl-btn" data-animation="fadeInUp" data-delay="1s" data-duration="500ms">Shop Now</a>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-
-		<!-- Single Slide Start -->
-		<div class="single_slide height-800 bg-img background-overlay" style="background-image: url(img/bg-img/bg-4.jpg);">
-			<div class="container h-100">
-				<div class="row h-100 align-items-center">
-					<div class="col-12">
-						<div class="welcome_slide_text">
-							<h6 data-animation="fadeInDown" data-delay="0" data-duration="500ms">* Only today we offer free shipping</h6>
-							<h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Summer Collection</h2>
-							<a href="#" class="btn karl-btn" data-animation="fadeInLeftBig" data-delay="1s" data-duration="500ms">Check Collection</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Single Slide Start -->
-		<div class="single_slide height-800 bg-img background-overlay" style="background-image: url(img/bg-img/bg-2.jpg);">
-			<div class="container h-100">
-				<div class="row h-100 align-items-center">
-					<div class="col-12">
-						<div class="welcome_slide_text">
-							<h6 data-animation="fadeInDown" data-delay="0" data-duration="500ms">* Only today we offer free shipping</h6>
-							<h2 data-animation="bounceInDown" data-delay="500ms" data-duration="500ms">Women Fashion</h2>
-							<a href="#" class="btn karl-btn" data-animation="fadeInRightBig" data-delay="1s" data-duration="500ms">Check Collection</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<?php } ?>
 	</div>
 </section>
 <!-- ****** Welcome Slides Area End ****** -->
-
-<!-- ****** Top Catagory Area Start ****** -->
-<section class="top_catagory_area d-md-flex clearfix">
-	<!-- Single Catagory -->
-	<div class="single_catagory_area d-flex align-items-center bg-img" style="background-image: url(img/bg-img/bg-2.jpg);">
-		<div class="catagory-content">
-			<h6>On Accesories</h6>
-			<h2>Sale 30%</h2>
-			<a href="#" class="btn karl-btn">SHOP NOW</a>
-		</div>
-	</div>
-	<!-- Single Catagory -->
-	<div class="single_catagory_area d-flex align-items-center bg-img" style="background-image: url(img/bg-img/bg-3.jpg);">
-		<div class="catagory-content">
-			<h6>in Bags excepting the new collection</h6>
-			<h2>Designer bags</h2>
-			<a href="#" class="btn karl-btn">SHOP NOW</a>
-		</div>
-	</div>
-</section>
-<!-- ****** Top Catagory Area End ****** -->
 
 <!-- ****** Quick View Modal Area Start ****** -->
 
@@ -223,7 +174,7 @@
 						<h6>Nunc pulvinar molestie sem id blandit. Nunc venenatis interdum mollis. Aliquam finibus nulla quam, a iaculis justo finibus non. Suspendisse in fermentum nunc.Nunc pulvinar molestie sem id blandit. Nunc venenatis interdum mollis. </h6>
 						<div class="testimonial-info d-flex align-items-center justify-content-center">
 							<div class="tes-thumbnail">
-								<img src="img/bg-img/tes-1.jpg" alt="">
+								<img src="<?= base_url() ?>frontend/img/bg-img/tes-1.jpg" alt="">
 							</div>
 							<div class="testi-data">
 								<p>Michelle Williams</p>
@@ -238,7 +189,7 @@
 						<h6>Nunc pulvinar molestie sem id blandit. Nunc venenatis interdum mollis. Aliquam finibus nulla quam, a iaculis justo finibus non. Suspendisse in fermentum nunc.Nunc pulvinar molestie sem id blandit. Nunc venenatis interdum mollis. </h6>
 						<div class="testimonial-info d-flex align-items-center justify-content-center">
 							<div class="tes-thumbnail">
-								<img src="img/bg-img/tes-1.jpg" alt="">
+								<img src="<?= base_url() ?>frontend/img/bg-img/tes-1.jpg" alt="">
 							</div>
 							<div class="testi-data">
 								<p>Michelle Williams</p>
@@ -253,7 +204,7 @@
 						<h6>Nunc pulvinar molestie sem id blandit. Nunc venenatis interdum mollis. Aliquam finibus nulla quam, a iaculis justo finibus non. Suspendisse in fermentum nunc.Nunc pulvinar molestie sem id blandit. Nunc venenatis interdum mollis. </h6>
 						<div class="testimonial-info d-flex align-items-center justify-content-center">
 							<div class="tes-thumbnail">
-								<img src="img/bg-img/tes-1.jpg" alt="">
+								<img src="<?= base_url() ?>frontend/img/bg-img/tes-1.jpg" alt="">
 							</div>
 							<div class="testi-data">
 								<p>Michelle Williams</p>
